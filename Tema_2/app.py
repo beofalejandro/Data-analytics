3f08560b7d7be90c9ff52c058414b1cd94d11916
Alejandro Barrientos <112975406+beofalejandro@users.noreply.github.com>
2025-03-26 22:51:35 -0600

    0.3 Stable - New Content on Tema_2

---
+++ Tema_2/app.py
@@ -0,0 +1,9 @@
+from pyswip import Prolog as Plg
+
+prolog = Plg()
+prolog.consult("hechos.pl")
+
+pregunta = input('¿Que le gusta a...? ')
+
+for valor in prolog.query("Le gusta (" + pregunta + ")."):
+    print(pregunta, "Le gusta a", valor['X'])
---
+++ Tema_2/hechos.pl
@@ -0,0 +1,11 @@
+le_gusta(juan,maria).
+le_gusta(juan,leer).
+le_gusta(maria,facebook).
+le_gusta(maria,cocinar).
+le_gusta(maria,musica).
+le_gusta(ines,musica).
+le_gusta(maria,juan).
+le_gusta(maria,nadar).
+
+juegan(juan,maria,ajedrez).
+juegan(teresa,mario,futbol).
